36a6f536d78d4434806da2f9118b586a51749f37
Peter Mekhail <peterfarouk01@yahoo.com>
2020-08-17 20:41:53 +1000

    Create progress_bar.py
    this is the first file

---
+++ progress_bar.py
@@ -0,0 +1,26 @@
+from tqdm import tqdm_gui
+from tqdm import tqdm
+import time
+import sys
+
+toolbar_width = 40
+
+# setup toolbar
+sys.stdout.write("[%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+for i in xrange(toolbar_width):
+    time.sleep(0.1) # do real work here
+    # update the bar
+    sys.stdout.write("#")
+    sys.stdout.flush()
+
+sys.stdout.write("]\n")
+
+
+myList = ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+
+for i in tqdm(myList):
+    time.sleep(2)
+    print(i)
