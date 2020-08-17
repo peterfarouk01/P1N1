9596bc3521d99731ace76f79a8c45df3d534f95f
Peter Mekhail <peterfarouk01@yahoo.com>
2020-08-17 21:32:12 +1000

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -10,7 +10,7 @@ sys.stdout.write("[%s]" % (" " * toolbar_width))
 sys.stdout.flush()
 sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
 
-for i in xrange(toolbar_width):
+for i in range(toolbar_width):
     time.sleep(0.1) # do real work here
     # update the bar
     sys.stdout.write("#")
